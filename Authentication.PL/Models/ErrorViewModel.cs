a006ab7147005d8c3c96dcee4099368f46874510
Abdalwanees <gabdo7682@gmail.com>
2024-09-26 17:14:01 +0300

    Add project files.

---
+++ Authentication.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Authentication.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
